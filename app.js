e66494c0f8b51b938fa24b9cafcf619ee17cfa03
S545389 <89318303+S545389@users.noreply.github.com>
2021-11-10 21:16:39 -0600

    updated

--- app.js
+++ app.js
@@ -4,11 +4,29 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const connectionString = process.env.MONGO_CON 
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString,{useNewUrlParser: true, useUnifiedTopology: true}); 
+
+//Get the default connection 
+var db = mongoose.connection; 
+ 
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+ console.log("Connection to DB succeeded")
+recreateDB();
+}); 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aeroplanesRouter = require('./routes/Aeroplanes');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
+const Costume = require("./models/costume");
+const resoureRouter = require('./routes/resource');
+var costumeRouter = require('./routes/costume');
+
 var app = express();
 
 // view engine setup
@@ -26,6 +44,8 @@ app.use('/users', usersRouter);
 app.use('/aeroplanes', aeroplanesRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
+app.use('/resource', resoureRouter);
+app.use('/costumes', costumeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +62,27 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+async function recreateDB(){ 
+  // Delete everything 
+  await Costume.deleteMany(); 
+ 
+  let instance1 = new Costume({costume_type:"ghost",  size:'large', cost:25.4}); 
+  let instance2 = new Costume({costume_type:"trojan war",  size:'Medium', cost:36.4}); 
+  let instance3 = new Costume({costume_type:"hounds of baskerville",  size:'small', cost:42.6}); 
 
+  instance1.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("First object saved") 
+  });
+  instance2.save( function(err,doc) { 
+    if(err) return console.error(err); 
+    console.log("second object saved") 
+  });
+  instance3.save( function(err,doc) { 
+    if(err) return console.error(err); 
+    console.log("third object saved") 
+  });
+  // let reseed = true; 
+  // if (reseed) { recreateDB();}  
+} 
 module.exports = app;
